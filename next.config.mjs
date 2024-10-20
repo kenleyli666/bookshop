9a87d1f1865733008a555c8704a70286cc5723c8
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-21 00:34:45 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,11 @@
+/** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
-        dest: 'public',
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === 'development',
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = {
-    ...nextConfig,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'kenleyli666.github.io',
-            },
-        ],
-    },
-};
+export default nextConfig;
