f7d7b04d65aa8d6e9ae756b381bdca2ec94a46c8
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-20 17:08:35 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 import withPWA from 'next-pwa';
+import runtimeCaching from 'next-pwa/cache';
 
 const nextConfig = withPWA({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
+    pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === 'development',
+        runtimeCaching,
+    },
+    images: {
+        domains: ['kenleyli666.github.io'],
+    },
 });
 
 export default nextConfig;
-
