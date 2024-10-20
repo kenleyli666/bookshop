b67acb58d47e8b3ae13f202545f657bfe46c8190
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-20 16:55:18 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 import withPWA from 'next-pwa';
-import runtimeCaching from 'next-pwa/cache.js'; 
 
 const nextConfig = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
-    runtimeCaching,
 });
 
 export default nextConfig;
