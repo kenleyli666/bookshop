28592b60aa21a8c756702c6931ed5e5dfb2e058b
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-21 14:48:52 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,33 @@
-import withPWA from 'next-pwa';
-import { createRequire } from 'module';
+// import withPWA from 'next-pwa';
+// import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
+// const require = createRequire(import.meta.url);
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'fakeimg.pl',
-      },
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'kenleyli666.github.io',
-      },
-    ],
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'fakeimg.pl',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'example.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'kenleyli666.github.io',
+//       },
+//     ],
+//   },
+// };
 
-const pwaConfig = withPWA({
-  dest: 'public',
-});
+// const pwaConfig = withPWA({
+//   dest: 'public',
+// });
 
-export default {
-  ...nextConfig,
-  ...pwaConfig,
-};
+// export default {
+//   ...nextConfig,
+//   ...pwaConfig,
+// };
